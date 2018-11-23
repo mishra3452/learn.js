26277331087da912b0ea659d90e1235950b01739
mishra3452 <mishrasumit.3452@gmail.com>
2018-11-23 21:29:22 +0530

    Loops Added

---
+++ 01GettingStarted/12Loops/01example.js
@@ -0,0 +1,26 @@
+let i = 3;
+
+while (i) {
+    console.log( i-- ); 
+}
+
+let j = 0;
+while (++j < 5) console.log( j );
+
+let k = 0;
+while (k++ < 5) console.log( k );
+/*
+Output :
+3
+2
+1
+1
+2
+3
+4
+1
+2
+3
+4
+5
+*/
