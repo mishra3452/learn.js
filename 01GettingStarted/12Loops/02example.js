e255c2eb99eba42a798e2a73e12196d77993591a
mishra3452 <mishrasumit.3452@gmail.com>
2018-11-23 21:37:00 +0530

    Loops Added

---
+++ 01GettingStarted/12Loops/02example.js
@@ -0,0 +1,30 @@
+//postfix form
+for (let i = 0; i < 5; i++) console.log( i );
+
+//prefix form
+for (let i = 0; i < 5; ++i) console.log( i );
+
+//odd numbers
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+      console.log( i );
+    }
+  }
+/*
+Output :
+0
+1
+2
+3
+4
+0
+1
+2
+3
+4
+2
+4
+6
+8
+10
+*/
