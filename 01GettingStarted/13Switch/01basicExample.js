5cbe009e839679d04498786a2b86442b1b76dac8
mishra3452 <mishrasumit.3452@gmail.com>
2018-11-24 19:17:42 +0530

    Switch Added

---
+++ 01GettingStarted/13Switch/01basicExample.js
@@ -0,0 +1,19 @@
+var i = 9; 
+  
+switch (i)  { 
+    case 0: 
+       document.write("i is zero."); 
+       break; 
+    case 1: 
+       document.write("i is one."); 
+       break; 
+    case 2: 
+       document.write("i is two."); 
+       break; 
+    default: 
+       document.write("i is greater than 2."); 
+} 
+/*
+Output : 
+i is greater than 2.
+*/
