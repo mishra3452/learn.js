9b42b7da748fe637a05a1932340b7f92e54e78f9
mishra3452 <mishrasumit.3452@gmail.com>
2018-11-26 21:40:37 +0530

    Functions in Go

---
+++ 01GettingStarted/14Functions/01basicExample.js
@@ -0,0 +1,18 @@
+/*
+This is basic example to understand the working of functions
+*/
+
+// Function definition 
+function welcomeMsg(name) { 
+    console.log("Hello " + name + " welcome to learn.js"); 
+ } 
+   
+ // creating a variable 
+ var nameVal = "Sumit"; 
+   
+ // calling the function 
+ welcomeMsg(nameVal); 
+
+ /*
+ Hello Sumit welcome to learn.js
+ */
